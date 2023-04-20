48e0683965afe396221cb16eb04c91eb64f6d2b7
ivaline <alina.iva.4@gmail.com>
2023-04-20 16:43:40 +0200

    test 1

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,7 @@
 package fizzbuzz;
 
 public class FizzBuzz {
-
+	public Object fizzbuzz(int i) {
+		return "1";
+	}
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
+	private 
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertEquals(FizzBuzz.fizzbuzz(1),"1");
 	}
 
 }
