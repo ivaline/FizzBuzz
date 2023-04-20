79ae910c99e7a144a2f96239c7001f74c9ee033d
ivaline <alina.iva.4@gmail.com>
2023-04-20 16:49:55 +0200

    Fizzbuzz(2) retourne 2

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
 	@Test
-	void test() {
+	void fizz_un() {
 		assertEquals(FizzBuzz.fizzbuzz(1),"1");
+	}
+	
+	@Test
+	void fizz_deux() {
+		assertEquals(FizzBuzz.fizzbuzz(2),"2");
 	}
 	
 }
